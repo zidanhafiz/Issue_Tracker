c8fc71cd4a97daddea8962c13685ef29a3736c9d
zidanhafiz <nadizsonata@gmail.com>
2023-12-17 16:36:14 +0700

    create get all issues api method

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,6 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
 import { createIssueSchema } from '@/app/validation-schema';
 
+export async function GET() {
+  try {
+    const res = await prisma.issue.findMany();
+    return NextResponse.json(res, { status: 200 });
+  } catch (err) {
+    console.error('Error:', err);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const validation = createIssueSchema.safeParse(body);
