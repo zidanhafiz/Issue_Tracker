920d57ff55bebaed5a89c4f38b0d18e5da53df4c
zidanhafiz <nadizsonata@gmail.com>
2023-12-17 12:46:37 +0700

    Add modal confirm on back button component

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,21 +1,65 @@
 'use client';
-import { Link } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { AlertDialog, Button, Flex, Link } from '@radix-ui/themes';
+import { usePathname, useRouter } from 'next/navigation';
 import { FaArrowLeft } from 'react-icons/fa';
 
 const BackButton = () => {
   const router = useRouter();
+  const currentPath = usePathname();
   const onClick = () => {
     router.back();
   };
+
+  if (currentPath !== '/issues/new') {
+    return (
+      <Link
+        onClick={onClick}
+        className='flex items-center gap-2'
+      >
+        <FaArrowLeft />
+        Back
+      </Link>
+    );
+  }
+
   return (
-    <Link
-      onClick={onClick}
-      className='flex items-center gap-2'
-    >
-      <FaArrowLeft />
-      Back
-    </Link>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Link className='flex items-center gap-2'>
+          <FaArrowLeft />
+          Back
+        </Link>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>Discard changes</AlertDialog.Title>
+        <AlertDialog.Description size='2'>
+          Are you sure? This changes will be discard.
+        </AlertDialog.Description>
+        <Flex
+          gap='3'
+          mt='4'
+          justify='end'
+        >
+          <AlertDialog.Cancel>
+            <Button
+              variant='soft'
+              color='gray'
+            >
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button
+              variant='solid'
+              color='red'
+              onClick={onClick}
+            >
+              Yes
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
