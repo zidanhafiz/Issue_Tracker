a1ba494b3b5b9fb3d7cbb523d66c7bd095ff3daa
zidanhafiz <nadizsonata@gmail.com>
2023-12-17 10:02:33 +0700

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,8 @@
 'use client';
-import { Button, TextFieldInput } from '@radix-ui/themes';
+import { Button, Callout, TextFieldInput } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
-import { error } from 'console';
 import { useRouter } from 'next/navigation';
 
 type IssueForm = {
@@ -12,7 +11,12 @@ type IssueForm = {
 };
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>();
   const router = useRouter();
 
   const sendData = async (data: IssueForm) => {
@@ -40,10 +44,15 @@ const NewIssuePage = () => {
       className='max-w-xl space-y-4'
       onSubmit={handleSubmit((data) => sendData(data))}
     >
+      {errors.title?.type === 'required' && <ErrorCallout message='Title is required!' />}
       <TextFieldInput
         placeholder='Title'
         {...register('title', { required: true })}
+        aria-invalid={errors.title ? 'true' : 'false'}
       />
+      {errors.description?.type === 'required' && (
+        <ErrorCallout message='Description is required!' />
+      )}
       <Controller
         name='description'
         control={control}
@@ -52,11 +61,23 @@ const NewIssuePage = () => {
           <SimpleMDE
             placeholder='Description'
             {...field}
+            aria-invalid={errors.description ? 'true' : 'false'}
           />
         )}
       />
       <Button>Create Issue</Button>
     </form>
+  );
+};
+
+const ErrorCallout = ({ message }: { message: string }) => {
+  return (
+    <Callout.Root
+      size='1'
+      color='red'
+    >
+      <Callout.Text>{message}</Callout.Text>
+    </Callout.Root>
   );
 };
 
